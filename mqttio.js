022508dbf99a099b48e1b62d34d0117828d69889
Ahrin Jun <wjsdkfls03@naver.com>
2024-03-31 23:55:02 +0900

    로그인 화면 전환 수정

--- mqttio.js
+++ mqttio.js
@@ -3,26 +3,16 @@ let connectionFlag = false; // 연결 상태이면 true
 let userName;
 
 function startConnect() { // 브로커에 접속하는 함수
-	if(connectionFlag == true)
-		return; // 현재 연결 상태이므로 다시 연결하지 않음
+	// 재로그인 시도 시
+	if(connectionFlag == true) {
+		subscribe("exist"); // 'exist' 토픽 구독
+		publish("check", userName); // 'check' 토픽으로 사용자 ID 발행
+	}
 
 	let broker = "localhost";
 	let port = 9001;
 	
 	userName = document.getElementById("getUserName").value.trim();
-
-	document.getElementById("userName").innerHTML += "<b>" + userName + "</b>";
-	document.getElementById("getUserName").style.display = "none";
-	document.getElementById("loginButton").style.display = "none";
-	document.getElementById("operateButton").innerHTML = "start";
-	for (let i = 0; i < hiddenItems.length; i++) {
-		hiddenItems[i].style.display = "inline";
-	}
-	
-	for (let i = 0; i < tables.length; i++) {
-		tables[i].style.display = "inline-table";
-	}
-	document.getElementById("total").innerHTML = "배출량: " + total;
 
 	// MQTT 메시지 전송 기능을 모두 가징 Paho client 객체 생성
 	client = new Paho.MQTT.Client(broker, Number(port), userName);
@@ -41,11 +31,9 @@ function startConnect() { // 브로커에 접속하는 함수
 		},
 		onFailure: function (error) {
 			document.getElementById("messages").innerHTML = `<span>Connection failed: ${error.errorMessage}</span><br/>`;
-            		connectionFlag = false;
+            connectionFlag = false;
 		}
 	});
-
-	document.documentElement.style.top = "92%"; // 화면 비율 조정
 }
 
 // 브로커로의 접속이 성공할 때 호출되는 함수
@@ -94,7 +82,25 @@ function onMessageArrived(msg) { // 매개변수 msg는 도착한 MQTT 메시지
 	document.getElementById("messages").innerHTML = '<span>메세지 도착: ' + msg.payloadString + '</span><br>';
 	if(msg.destinationName == "exist") {
 		if(msg.payloadString == "false") {
+			unsubscribe("exist");
+			userName = "";
 			document.getElementById("messages").innerHTML = "<span>등록되지 않은 사용자입니다. 다시 입력해주세요. </span><br>"
+			document.documentElement.style.top = "73%";
+		}
+		else if(msg.payloadString == "true") {
+			document.getElementById("userName").innerHTML += "<b>" + userName + "</b>";
+			document.getElementById("getUserName").style.display = "none";
+			document.getElementById("loginButton").style.display = "none";
+			document.getElementById("operateButton").innerHTML = "start";
+			for (let i = 0; i < hiddenItems.length; i++) {
+				hiddenItems[i].style.display = "inline";
+			}
+			
+			for (let i = 0; i < tables.length; i++) {
+				tables[i].style.display = "inline-table";
+			}
+			document.getElementById("total").innerHTML = "배출량: " + total;
+			document.documentElement.style.top = "92%"; // 화면 비율 조정
 		}
 	}
 	else {
