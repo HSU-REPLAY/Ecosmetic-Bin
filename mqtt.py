783b2423ddda62b90f37f7658d5fbcb14a62c053
YseungY <147280336+YseungY@users.noreply.github.com>
2024-04-02 04:37:37 +0900

    Add files via upload
    id check DB 파일 분리

--- mqtt.py
+++ mqtt.py
@@ -1,6 +1,8 @@
 #mqtt.py
 import paho.mqtt.client as mqtt
 import camocr
+import json
+from database import check_user_exists
 
 def on_connect(client, userdata, flag, rc):
     client.subscribe("check")  # "check" 토픽으로 구독 신청
@@ -14,16 +16,12 @@ def on_message(client, userdata, msg):
         client.publish("exist", "true" if user_exists else "false")
 
     elif msg.topic == "start_camera":
-        # 'start' 버튼을 누르고, 사용자가 존재하면 카메라 작동
-        camocr.cam()  # 카메라를 활성화하는 함수
+        # 사용자가 'start' 버튼을 누르고, 사용자가 존재한다면 카메라 작동
+        camocr.cam()  # 카메라를 활성화하는 함수를 호출
         new_payload = str(msg.payload.decode()) + ',' + camocr.getCnt()
 	# 새로운 문자열을 새로운 토픽으로 발행
         client.publish("push", new_payload)
         print("publish(\"push\", ", new_payload, ")")
-
-# 가상DB - 실제 DB 로직으로 대체해야함
-def check_user_exists(user_id):
-    return user_id in ["user1", "user2", "user3"]
 
 ip = "localhost" # 현재 브로커는 이 컴퓨터에 설치되어 있음
 
@@ -34,4 +32,5 @@ client.on_message = on_message
 
 client.connect(ip, 1883) # 브로커에 연결
 client.loop_forever() # 메시지 루프
+
 
